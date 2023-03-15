cf8a4d0bfeea4ee4d684b3b83747f03ce9928e4a
young-trigold <cluscandlot63@gmail.com>
2023-03-15 09:35:08 +0800

    fix: 数据

--- apps/database/src/script.ts
+++ apps/database/src/script.ts
@@ -2,14 +2,31 @@ import { connectDataBase } from './connect.js';
 import { Record } from './index.js';
 
 await connectDataBase('爬虫');
-await Record.updateMany({ date: { $ne: '2023-02-09' } }, [
-  {
-    $set: {
-      endPrice: '$averagePrice',
-      maxPrice: '$endPrice',
-      averagePrice: '$maxPrice',
-    },
-  },
-]);
+// - 海峡 /福建海峡 update 海峡
+const records = await Record.find({ agency: { $regex: /海峡/ } });
 
-console.log('更新完成');
+const agencyToCountMap = records
+  .map((record) => record.agency)
+  .reduce(
+    (result, agency) => result.set(agency, result.get(agency) ? result.get(agency)! + 1 : 1),
+    new Map<string, number>(),
+  );
+
+console.log(agencyToCountMap);
+
+for (const key of agencyToCountMap.keys()) {
+  console.log(key);
+  const records = await Record.find({ agency: key });
+  const years = records.map((record) => record.date.slice(0, 4));
+  const yearToCountMap = years.reduce(
+    (result, year) => result.set(year, result.get(year) ? result.get(year)! + 1 : 1),
+    new Map<string, number>(),
+  );
+  console.log(yearToCountMap);
+}
+
+// - 欧洲 remove
+// await Record.remove({agency: {$regex: /欧洲/}});
+// const records = await Record.find({agency: {$regex: /欧洲/}});
+// console.log(records.length);
+// console.log('完成!');
--- apps/server/src/controller/record.ts
+++ apps/server/src/controller/record.ts
@@ -8,7 +8,7 @@ export const getRecords = async (req: Request, res: Response) => {
   const agencies = (query?.checkedAgencies as string)?.split(',') ?? ['上海', '湖北', '深圳', '广州'];
   const key = (query?.key as QueryKeyOfCarbonBusiness) ?? 'averagePrice';
   try {
-    const records = await Record.aggregate([
+    const recordsByDate = await Record.aggregate([
       {
         $match: {
           date: {
@@ -27,14 +27,13 @@ export const getRecords = async (req: Request, res: Response) => {
         },
       },
     ]);
-    let results = records.map((record) => {
-      const result: any = { date: record.date };
-      (record.records as CarbonBusiness[]).forEach((record) => {
-        const agency = record.agency.slice(0, 2);
-        if (agencies && agencies.length && agencies.includes(agency)) {
-          result[agency] = record[key];
-        } else {
-          result[agency] = record[key];
+    let results = recordsByDate.map((recordByDate) => {
+      const result: any = { date: recordByDate.date };
+      (recordByDate.records as CarbonBusiness[]).forEach((record) => {
+        const rawAgencyOfRecord = record.agency.slice(0, 2);
+        const agencyOfRecord = rawAgencyOfRecord === '福建' ? '海峡' : rawAgencyOfRecord;
+        if (agencies && agencies.length && agencies.includes(agencyOfRecord)) {
+          result[agencyOfRecord] = record[key];
         }
       });
       return result;
